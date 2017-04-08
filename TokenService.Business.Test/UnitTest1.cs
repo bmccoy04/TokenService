40d43c1e701c8c173287f1c423126b46a6a8ee26
bmccoy04 <bryanmccoy04@gmail.com>
2017-04-07 19:53:52 -0500

    first commit

---
+++ TokenService.Business.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TokenService.Business.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ TokenService.Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TokenService.Business
+{
+    public class Class1
+    {
+    }
+}
